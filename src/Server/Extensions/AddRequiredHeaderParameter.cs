6f4cc76db7313b4bb6df116dcd4264e294274327
Ahmed Magdy <ahmedmagdy4122@gmail.com>
2022-06-22 00:20:47 +0200

    Add Company Header

---
+++ src/Server/Extensions/AddRequiredHeaderParameter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+
+namespace ErpDashboard.Server.Extensions
+{
+    public class AddRequiredHeaderParameter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters == null)
+                operation.Parameters = new List<OpenApiParameter>();
+
+            operation.Parameters.Add(new OpenApiParameter
+            {
+                Name = "Company",
+                In = ParameterLocation.Header,
+                Description = "Company Id",
+                Required = true
+            });
+        }
+    }
+}
