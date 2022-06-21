cf50ea00abe497aa50268682ee887404bcd31cfd
MarwaGamal4 <69363116+MarwaGamal4@users.noreply.github.com>
2022-06-22 00:09:13 +0200

    update item department

---
+++ src/Application/Features/ItemsDepartments/Queries/GetAll/GetAllItemDepartmentQuery.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using ErpDashboard.Application.Features.ItemsDepartments.Queries.Dto;
+using ErpDashboard.Application.Interfaces.Repositories;
+using ErpDashboard.Application.Models;
+using ErpDashboard.Shared.Wrapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ErpDashboard.Application.Features.ItemsDepartments.Queries.GetAll
+{
+    public class GetAllItemDepartmentQuery : IRequest<Result<List<GetItemDepartmentResponse>>>
+    {
+        public GetAllItemDepartmentQuery()
+        {
+
+        }
+
+    }
+    internal class GetAllItemDepartmentHandler : IRequestHandler<GetAllItemDepartmentQuery, Result<List<GetItemDepartmentResponse>>>
+    {
+        private readonly ICustomIUnitOfWork<int> _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetAllItemDepartmentHandler(ICustomIUnitOfWork<int> unitOfWork , IMapper mapper)
+        {
+
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public async Task<Result<List<GetItemDepartmentResponse>>> Handle(GetAllItemDepartmentQuery request, CancellationToken cancellationToken)
+        {
+            var depts = await _unitOfWork.Repository<TbDepartment>().GetAllAsync();
+            var MappedResult = _mapper.Map<List<GetItemDepartmentResponse>>(depts);
+            return await Result<List<GetItemDepartmentResponse>>.SuccessAsync(MappedResult);
+
+        }
+    }
+}
---
+++ src/Application/Features/ItemsDepartments/Queries/GetById/GetItemDepartmentByIdQuery.cs
@@ -0,0 +1,37 @@
+﻿using ErpDashboard.Shared.Wrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ErpDashboard.Application.Features.ItemsDepartments.Queries.Dto;
+using MediatR;
+using System.Threading;
+using ErpDashboard.Application.Interfaces.Repositories;
+using AutoMapper;
+using ErpDashboard.Application.Models;
+
+namespace ErpDashboard.Application.Features.ItemsDepartments.Queries.GetById
+{
+    internal class GetItemDepartmentByIdQuery : IRequest<Result<GetItemDepartmentResponse>>
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+    internal class GetItemDepartmentByIdQueryHandler : IRequestHandler<GetItemDepartmentByIdQuery, Result<GetItemDepartmentResponse>>
+    {
+        private readonly ICustomIUnitOfWork<int> _unitOfWork;
+        private readonly IMapper _mapper;
+        public GetItemDepartmentByIdQueryHandler(ICustomIUnitOfWork<int> unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public async Task<Result<GetItemDepartmentResponse>> Handle(GetItemDepartmentByIdQuery request, CancellationToken cancellationToken)
+        {
+            var dept = _unitOfWork.Repository<TbDepartment>().GetByIdAsync(request.id);
+            var MapedDept =  _mapper.Map<GetItemDepartmentResponse>(dept);
+            return await Result<GetItemDepartmentResponse>.Success(MapedDept);
+        }
+    }
+}
