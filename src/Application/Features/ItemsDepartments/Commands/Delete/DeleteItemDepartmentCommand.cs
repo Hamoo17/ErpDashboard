6c53981808dc78d512b80b6b28e10213bc697670
MarwaGamal4 <69363116+MarwaGamal4@users.noreply.github.com>
2022-06-21 14:44:01 +0200

    Add Department Crud

---
+++ src/Application/Features/ItemsDepartments/Commands/Delete/DeleteItemDepartmentCommand.cs
@@ -0,0 +1,56 @@
+﻿using ErpDashboard.Application.Interfaces.Repositories;
+using ErpDashboard.Application.Models;
+using ErpDashboard.Shared.Wrapper;
+using MediatR;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ErpDashboard.Application.Features.ItemsDepartments.Commands.Delete
+{
+    public class DeleteItemDepartmentCommand : IRequest<Result<int>>
+    {
+        public int Id { get; set; }
+    }
+    internal class DeleteItemDepartmentCommandHandler : IRequestHandler<DeleteItemDepartmentCommand, Result<int>>
+    {
+        private readonly IItemDepartment _IItemDepartment;
+        private readonly IStringLocalizer<DeleteItemDepartmentCommand> _localizer;
+        private readonly ICustomIUnitOfWork<int> _customIUnitOf;
+        public DeleteItemDepartmentCommandHandler(ICustomIUnitOfWork<int> customIUnitOf , IItemDepartment IItemDepartment, IStringLocalizer<DeleteItemDepartmentCommand> localizer)
+        {
+
+            _customIUnitOf = customIUnitOf;
+            _IItemDepartment = IItemDepartment;
+            _localizer = localizer;
+
+        }
+        public async Task<Result<int>> Handle(DeleteItemDepartmentCommand command, CancellationToken cancellationToken)
+        {
+            var deptIsUsed = await _IItemDepartment.IsDepartmentUsed(command.Id);
+            if(!deptIsUsed)
+            {
+                var dept = await _customIUnitOf.Repository<TbDepartment>().GetByIdAsync(command.Id);
+                if(dept != null)
+                {
+                    await _customIUnitOf.Repository<TbDepartment>().DeleteAsync(dept);
+                    await _customIUnitOf.Commit(cancellationToken);
+                    return await Result<int>.SuccessAsync(command.Id, _localizer["Department Deleted"]);
+                }
+                else
+                {
+                    return await Result<int>.FailAsync( _localizer["Department NotFound"]);
+                }
+            }
+            else
+            {
+                return await Result<int>.FailAsync(_localizer["Deletion Not Allowed"]);
+
+            }
+        }
+    }
+}
---
+++ src/Application/Interfaces/Repositories/IItemDepartment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErpDashboard.Application.Interfaces.Repositories
+{
+    internal interface IItemDepartment
+    {
+        Task<bool> IsDepartmentExist(int dept_id);
+        Task<bool> IsDepartmentUsed(int dept_id);
+
+
+    }
+}
