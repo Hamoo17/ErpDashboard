a84c7d977d4433dff344c2361ac9620d6fec5b95
MarwaGamal4 <69363116+MarwaGamal4@users.noreply.github.com>
2022-06-24 23:04:02 +0200

    add item (addedit , delete , getall , getbyid , controller)

---
+++ src/Application/Mappings/ItemProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using ErpDashboard.Application.Features.Items.Commands.AddEdit;
+using ErpDashboard.Application.Features.Items.Quaries.Dto;
+using ErpDashboard.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErpDashboard.Application.Mappings
+{
+    public class ItemProfile : Profile
+    {
+        public ItemProfile()
+        {
+            CreateMap<AddEditItemCommand, TbItem>().ReverseMap();
+            CreateMap<GetItemResponse, TbItem>().ReverseMap();
+
+        }
+    }
+}
---
+++ src/Server/Controllers/v1/ItemController.cs
@@ -0,0 +1,65 @@
+﻿using ErpDashboard.Application.Features.Items.Commands.AddEdit;
+using ErpDashboard.Application.Features.Items.Commands.Delete;
+using ErpDashboard.Application.Features.Items.Quaries.GetAll;
+using ErpDashboard.Application.Features.Items.Quaries.GetById;
+using ErpDashboard.Shared.Constants.Permission;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ErpDashboard.Server.Controllers.v1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : BaseApiController<ItemController>
+    {
+        /// <summary>
+        /// Create/Update Item
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns>Status 200 OK</returns>
+        [Authorize(Policy = Permissions.Item.Create)]
+        [HttpPost]
+        public async Task<IActionResult> Post(AddEditItemCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+
+        /// <summary>
+        /// Delete Item
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Status 200 OK</returns>
+        [Authorize(Policy = Permissions.Item.Delete)]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok(await _mediator.Send(new DeleteItemCommand { Id = id }));
+        }
+
+        /// <summary>
+        /// Get All Items
+        /// </summary>
+        /// <returns>Status 200 OK</returns>
+        [Authorize(Policy = Permissions.Item.View)]
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var MealCategories = await _mediator.Send(new GetAllItemsQuery());
+            return Ok(MealCategories);
+        }
+
+        /// <summary>
+        /// Get Items By Id
+        /// </summary>
+        /// <param name="id">Id To Get</param>
+        /// <returns>Status 200 OK</returns>
+        [Authorize(Policy = Permissions.Item.View)]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var MealCategory = await _mediator.Send(new GetItemByIdQuery() { Id = id });
+            return Ok(MealCategory);
+        }
+    }
+}
