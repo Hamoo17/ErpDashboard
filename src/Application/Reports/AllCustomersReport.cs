63b39809a4325b0384f9025e6d10231069e41d46
Ahmed Magdy <ahmedmagdy4122@gmail.com>
2022-06-26 14:46:03 +0200

    Add New Pdf Viewer

--- src/Application/Reports/AllCustomersReport.cs
+++ src/Application/Reports/AllCustomersReport.cs
@@ -8,5 +8,16 @@ using ErpDashboard.Application.Models;
 
 namespace ErpDashboard.Application.Reports
 {
-   
+   public class AllCustomersReport 
+    {
+        ICustomIUnitOfWork<int> _unitofWork;
+        public AllCustomersReport(ICustomIUnitOfWork<int> unitofWork)
+        {
+            _unitofWork = unitofWork;
+        }
+        public  List<TbCustomer> CustomersList() 
+        {
+            return _unitofWork.Repository<TbCustomer>().Entities.ToList();
+        }
+    }
 }
--- src/Client/wwwroot/js/webviewerScripts.js
+++ src/Client/wwwroot/js/webviewerScripts.js
@@ -1,11 +1,36 @@
-﻿window.webviewerFunctions = {
-    initWebViewer: function () {
+﻿window.Download = (options) => {
+    var fileUrl = "data:" + options.mimeType + ";base64," + options.byteArray;
+   // console.log(fileUrl);
+    fetch(fileUrl)
+        .then(response => response.blob())
+        .then(blob => {
+            var link = window.document.createElement("a");
+            link.href = window.URL.createObjectURL(blob, { type: options.mimeType });
+           // link.download = options.fileName;
+           // document.body.appendChild(link);
+           // link.click();
+           // document.body.removeChild(link);
+            console.log('The Link Is => '+link.href);
+            webviewerFunctions.initWebViewer(blob);
+            return link.href;
+        });
+    
+}
+
+window.webviewerFunctions = {
+    initWebViewer: function (blob) {
+
         const viewerElement = document.getElementById('viewer');
         WebViewer({
             path: 'lib',
-            initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf', // replace with your own PDF file
+            extension: 'pdf',
         }, viewerElement).then((instance) => {
-            // call apis here
+            instance.UI.loadDocument(blob, { filename: 'myfile.pdf' });
+            //instance.initWebViewer();
+            const { documentViewer } = instance.Core;
+            documentViewer.addEventListener('documentLoaded', () => {
+                // perform document operations
+            });
         })
     }
 };
