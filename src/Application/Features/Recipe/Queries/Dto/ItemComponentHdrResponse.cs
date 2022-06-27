8c3f1cc4a90280db8b27b147e4eda57033a3a905
MarwaGamal4 <69363116+MarwaGamal4@users.noreply.github.com>
2022-06-27 11:38:15 +0200

    add items (add edit operations)

---
+++ src/Application/Features/Recipe/Queries/Dto/ItemComponentHdrResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErpDashboard.Application.Features.Recipe.Queries.Dto
+{
+    public class ItemComponentHdrResponse
+    {
+        public int Id { get; set; }
+        public int ComplexItemId { get; set; }
+        public decimal QtyNeeded { get; set; }
+        public int MainUnit { get; set; }
+
+        public List<ItemComponentDetailResponse> itemComponentDetailResponse { get; set; }
+
+    }
+}
