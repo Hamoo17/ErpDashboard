234fb7cac178ebd590ae603a1703e0d3a45ed0bc
MarwaGamal4 <69363116+MarwaGamal4@users.noreply.github.com>
2022-07-02 13:27:00 +0200

    Add Categories (Add, Edit , GetAll)

---
+++ src/Application/Features/Categories/Queries/GetAll/CategoryResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErpDashboard.Application.Features.Categories.Queries.GetAll
+{
+    public class CategoryResponse
+    {
+        public int Id { get; set; }
+        public string EnName { get; set; }
+    }
+}
---
+++ src/Server/Controllers/v1/CategoryController.cs
@@ -0,0 +1,48 @@
+﻿using ErpDashboard.Application.Features.Categories.Command.AddEdit;
+using ErpDashboard.Application.Features.Categories.Command.Delete;
+using ErpDashboard.Application.Features.Categories.Queries.GetAll;
+using ErpDashboard.Shared.Constants.Permission;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ErpDashboard.Server.Controllers.v1
+{
+    [ApiController]
+    public class CategoryController : BaseApiController<CategoryController>
+    {
+        /// <summary>
+        /// Create/Update a  Category
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns>Status 200 OK</returns>
+        [Authorize(Policy = Permissions.Category.Create)]
+        [HttpPost]
+        public async Task<IActionResult> Post(AddEditCategoryCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+        /// <summary>
+        /// Delete  Category
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Status 200 OK</returns>
+        [Authorize(Policy = Permissions.Category.Delete)]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok(await _mediator.Send(new DeleteCategoryCommand { Id = id }));
+        }
+        /// <summary>
+        /// Get All Items (List of CategoryResponse)
+        /// </summary>
+        /// <returns>Status 200 OK</returns>
+        //[Authorize(Policy = Permissions.Category.View)]
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var Items = await _mediator.Send(new GetAllCategoriesQuery());
+            return Ok(Items);
+        }
+    }
+}
