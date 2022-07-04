96eec052da5d9815e12b638a6df0d720f145f57c
MarwaGamal4 <69363116+MarwaGamal4@users.noreply.github.com>
2022-07-04 12:42:13 +0200

    Add Meals Category Page

---
+++ src/Client.Infrastructure/Managers/MealsCategories/IMealsCategoriesManager.cs
@@ -0,0 +1,20 @@
+﻿using ErpDashboard.Application.Features.MealsCategory.Commands.AddEdit;
+using ErpDashboard.Application.Features.MealsCategory.Queries.Dto;
+using ErpDashboard.Shared.Wrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErpDashboard.Client.Infrastructure.Managers.MealsCategories
+{
+    public interface IMealsCategoriesManager : IManager
+    {
+         Task<IResult<int>> SaveAsync(AddEditMealsCategoryCommand command);
+         Task<IResult<int>> DeleteAsync(int id);
+         Task<IResult<GetMealCategoryResponse>> GetByIdAsync(int id);
+         Task<IResult<List<GetMealCategoryResponse>>> GetAllAsync();
+
+    }
+}
---
+++ src/Client.Infrastructure/Routes/MealsCategoriesEndPoints.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErpDashboard.Client.Infrastructure.Routes
+{
+    public class MealsCategoriesEndPoints
+    {
+        public static string GetAll = "api/V1/MealsCategories";
+        public static string Save = "api/V1/MealsCategories";
+        public static string GetById(int id) => $"api/V1/MealsCategories/{id}";
+        public static string Delete(int id) => $"api/V1/MealsCategories/{id}";
+    }
+}
