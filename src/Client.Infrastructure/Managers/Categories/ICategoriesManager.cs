47f3b5fe018e236933fac7af66b8cca476bca481
MarwaGamal4 <69363116+MarwaGamal4@users.noreply.github.com>
2022-07-04 11:13:21 +0200

    Add Category

---
+++ src/Client.Infrastructure/Managers/Categories/ICategoriesManager.cs
@@ -0,0 +1,20 @@
+﻿using ErpDashboard.Application.Features.Categories.Command.AddEdit;
+using ErpDashboard.Application.Features.Categories.Queries.GetAll;
+using ErpDashboard.Shared.Wrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErpDashboard.Client.Infrastructure.Managers.Categories
+{
+
+    public interface ICategoriesManager : IManager
+    {
+        public Task<IResult<int>> SaveAsync(AddEditCategoryCommand command);
+        public Task<IResult<int>> DeleteAsync(int id);
+
+        public Task<IResult<List<CategoryResponse>>> GetAllAsync();
+    }
+}
---
+++ src/Client.Infrastructure/Routes/CategoriesEndPoints.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErpDashboard.Client.Infrastructure.Routes
+{
+    public class CategoriesEndPoints
+    {
+        public static string GetAll = "api/V1/Categories";
+        public static string Save = "api/V1/Categories";
+        public static string Delete(int id) => $"api/V1/Categories/{id}";
+    }
+}
