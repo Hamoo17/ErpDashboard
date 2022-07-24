63ab8fa2f032b8eeb356b2b319c130733c60142c
MarwaGamal4 <69363116+MarwaGamal4@users.noreply.github.com>
2022-07-24 17:13:31 +0200

    add edit delete units

---
+++ src/Client/Pages/Units/AddEditUnitsModal.razor.cs
@@ -0,0 +1,37 @@
+﻿using Blazored.FluentValidation;
+using ErpDashboard.Application.Features.Units.Commands.AddEdit;
+using ErpDashboard.Client.Infrastructure.Managers.Units;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace ErpDashboard.Client.Pages.Units
+{
+    public partial class AddEditUnitsModal
+    {
+        [CascadingParameter] MudDialogInstance MudDialog { get; set; }
+        [Inject] public IUnitsManager _planManager { get; set; }
+        [Parameter] public AddEditUnitsCommand Model { get; set; } = new();
+        private FluentValidationValidator _fluentValidationValidator;
+        private bool Validated => _fluentValidationValidator.Validate(options => { options.IncludeAllRuleSets(); });
+        private async Task Save()
+        {
+            var Response = await _planManager.SaveAsync(Model);
+            if (Response.Succeeded)
+            {
+                _snackBar.Add(Response.Messages[0], MudBlazor.Severity.Success);
+                MudDialog.Close();
+            }
+            else
+            {
+                foreach (var msg in Response.Messages)
+                {
+                    _snackBar.Add(msg, MudBlazor.Severity.Error);
+                }
+            }
+        }
+        public void Cancel()
+        {
+            MudDialog.Cancel();
+        }
+    }
+}
