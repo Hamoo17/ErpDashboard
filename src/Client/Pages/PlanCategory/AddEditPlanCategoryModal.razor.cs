976302c691f5f7e2a6d8ba7a4c3ce91193d945a8
MarwaGamal4 <69363116+MarwaGamal4@users.noreply.github.com>
2022-07-24 16:42:26 +0200

    add edit delete plancategory

---
+++ src/Client/Pages/PlanCategory/AddEditPlanCategoryModal.razor.cs
@@ -0,0 +1,37 @@
+﻿using Blazored.FluentValidation;
+using ErpDashboard.Application.Features.PlanCategory.Command.AddEdit;
+using ErpDashboard.Client.Infrastructure.Managers.PlanCategory;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace ErpDashboard.Client.Pages.PlanCategory
+{
+    public partial class AddEditPlanCategoryModal
+    {
+        [CascadingParameter] MudDialogInstance MudDialog { get; set; }
+        [Inject] public IPlanCtegoryManager _planManager { get; set; }
+        [Parameter] public AddEditPlanCategoryCommand Model { get; set; } = new();
+        private FluentValidationValidator _fluentValidationValidator;
+        private bool Validated => _fluentValidationValidator.Validate(options => { options.IncludeAllRuleSets(); });
+        private async Task Save()
+        {
+            var Response = await _planManager.SaveAsync(Model);
+            if (Response.Succeeded)
+            {
+                _snackBar.Add(Response.Messages[0], MudBlazor.Severity.Success);
+                MudDialog.Close();
+            }
+            else
+            {
+                foreach (var msg in Response.Messages)
+                {
+                    _snackBar.Add(msg, MudBlazor.Severity.Error);
+                }
+            }
+        }
+        public void Cancel()
+        {
+            MudDialog.Cancel();
+        }
+    }
+}
